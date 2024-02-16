19402ae12a64171eaea5c3109bc850869b6a3377
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-02-16 12:32:20 +0600

    clock created

---
+++ script.js
@@ -0,0 +1,64 @@
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+
+function newDate() {
+    let date = new Date();
+    let d = date.getDate();
+    let year = date.getFullYear();
+    let month = months[date.getMonth()];
+    let day = days[date.getDay()];
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = 'AM';
+    if (m <= 9) {
+        m = '0' + m;
+    }
+    if (s <= 9) {
+        s = '0' + s;
+    }
+    if (h <= 9) {
+        h = '0' + h;
+    }
+    if (h > 11) {
+        session = 'PM'
+    }
+    if (h > 12) {
+        h = h - 12;
+    }
+
+    const time = h + ':' + m + ':' + s + ' ' + session;
+    document.getElementById('clock').innerText = time;
+
+    const today = month + ' ' + d + ', ' + year;
+    document.getElementById('tarikh').innerText = today;
+    document.getElementById('today').innerHTML = day;
+    console.log(s);
+    setTimeout("newDate()", 100);
+}
+
+newDate();
